ee7ab30301154236c5adc4cb0e21ef1e70024def
3116005131 <duanhaobin@icloud.com>
2019-03-21 20:42:00 +0800

    1.0 ok

---
+++ currency_converter_v1.0.py
@@ -0,0 +1,16 @@
+"""
+    作者：段浩彬
+    功能：货币兑换
+    版本：1.0
+    日期：2019/3/21
+"""
+
+# 汇率
+USD_VS_RMB = 6.77
+
+# 人民币输入
+currency_str_value = input("请输入带单位的货币金额")
+
+# 拿第一个字符
+kind = currency_str_value[0]
+
