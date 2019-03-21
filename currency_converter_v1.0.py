fd7f14dfa70550ab843a6b32bf5a1b77dd67ab2f
3116005131 <duanhaobin@icloud.com>
2019-03-21 21:12:56 +0800

    2.0

--- currency_converter_v1.0.py
+++ currency_converter_v1.0.py
@@ -11,6 +11,20 @@ USD_VS_RMB = 6.77
 # 人民币输入
 currency_str_value = input("请输入带单位的货币金额")
 
-# 拿第一个字符
-kind = currency_str_value[0]
+# 获取货币单位
+unit = currency_str_value[-3:]
 
+if unit == "CNY":
+    # 获取的是人民币
+    rmb_str_value = currency_str_value[:-3]
+    rmb_value = eval(rmb_str_value)
+    usd_value = rmb_value / USD_VS_RMB
+    print("美元金额（USD）：", usd_value)
+elif unit == "USD":
+    # 获取的是美元
+    usd_str_value = currency_str_value[:-3]
+    usd_value = eval(usd_str_value)
+    rmb_value = usd_value * USD_VS_RMB
+    print("人民币金额（CNY）:", rmb_value)
+else:
+    print("目前版本尚不支持该种货币")
