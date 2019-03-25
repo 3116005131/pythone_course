4da8c823e1513781a4235b24f52b5a55eaa15c1c
3116005131 <duanhaobin@icloud.com>
2019-03-25 20:52:09 +0800

    no chinese?

--- currency_converter_v1.0.py
+++ currency_converter_v1.0.py
@@ -6,32 +6,43 @@
 """
 
 
-def convert_currency(im, er):
+# def convert_currency(im, er):
+#     """
+#         汇率兑换函数
+#     """
+#     out = im * er
+#     return out
+
+
+def main():
     """
-        汇率兑换函数
+        主函数
     """
-    out = im * er
-    return out
+
+    # 汇率
+    USD_VS_RMB = 6.77
+
+    # 带单位的货币输入
+    currency_str_value = input("请输入带单位的货币金额")
+
+    unit = currency_str_value[-3:]
+    if unit == "CNY":
+        exchange_rate = USD_VS_RMB
+    elif unit == "USD":
+        exchange_rate = 1 / USD_VS_RMB
+    else:
+        exchange_rate = -1
+
+    if exchange_rate != -1:
+        in_money = eval(currency_str_value[:-3])
+        # 使用lambda定义函数
+        convert_currency = lambda x: x * exchange_rate
+
+        out_money = convert_currency(in_money)
+        print('转换后的金额', out_money)
+    else:
+        print('不支持该种货币')
 
 
-# 汇率
-USD_VS_RMB = 6.77
-currency_str_value = input("请输入带单位的货币金额")
-# 获取货币单位
-unit = currency_str_value[-3:]
-if unit == "CNY":
-    exchange_rate = USD_VS_RMB
-elif unit == "USD":
-    exchange_rate = 1 / USD_VS_RMB
-else:
-    exchange_rate = -1
-
-if exchange_rate != -1:
-    in_money = eval(currency_str_value[:-3])
-    # 调用函数
-    out_money = convert_currency(in_money, exchange_rate)
-    print('转换后的金额', out_money)
-else:
-    print('不支持该种货币')
-
-print('程序已退出')
+if __name__ == '__main__':
+    main()
